b2abda9cf302e6e199a6545dbff7834c9e5cd1cb
Feraspar <97383817+Feraspar@users.noreply.github.com>
2024-07-09 23:15:15 +0700

    Create AddButton

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -8,6 +8,8 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using To_Do_List_WPF.Data;
+using To_Do_List_WPF.Models;
 
 namespace To_Do_List_WPF
 {
@@ -16,9 +18,36 @@ namespace To_Do_List_WPF
     /// </summary>
     public partial class MainWindow : Window
     {
+        private ToDoContext _context;
         public MainWindow()
         {
             InitializeComponent();
+            _context = new ToDoContext();
+            LoadTodoItems();
+        }
+
+        private void LoadTodoItems()
+        {
+            var Todos = _context.ToDoItems.ToList();
+            ToDoListBox.ItemsSource = Todos;
+        }
+
+        private void AddButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (!string.IsNullOrEmpty(ToDoItemTextBox.Text))
+            {
+                var newTodoItem = new ToDoItem { Title = ToDoItemTextBox.Text, IsCompleted = false };
+                _context.ToDoItems.Add(newTodoItem);
+                _context.SaveChanges();
+
+                LoadTodoItems();
+                ToDoItemTextBox.Clear();
+            }
+        }
+
+        private void DeleteButton_Click(object sender, RoutedEventArgs e)
+        {
+            
         }
     }
 }
